826f1f97ab02d4bdfbf81be6273d69b7ebc73645
Blackbox141 <Dennis.langer@students.fhnw.ch>
2024-03-04 09:22:59 +0100

    test 2

---
+++ UserInput.py
@@ -0,0 +1,9 @@
+def search_hotel():
+    num_days = float(input("Enter how many days"))
+    UF = 1*num_days
+    return num_days
+
+numberofdays = search_hotel()
+print(numberofdays)
+
+print("TEST UPDATE GITHUB")
