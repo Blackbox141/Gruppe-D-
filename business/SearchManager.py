c484c22ddc6e97042b7ad316396baba75d8cdee3
Blackbox141 <Dennis.langer@students.fhnw.ch>
2024-05-06 08:53:10 +0200

    latest update

--- business/SearchManager.py
+++ business/SearchManager.py
@@ -1,62 +1,11 @@
 # include all search functions here
 # accept search criteria, search by various criteria
-from data_access.data_base import init_db
-from data_models.models import *
-from pathlib import Path
-from sqlalchemy.orm.scoping import scoped_session
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import create_engine
-from sqlalchemy import select
 
-
-class SearchManager(object):
-    def __init__(self, database_file):
-        database_path = Path(database_file)
-        if not database_path.is_file():
-            init_db(database_file, generate_example_data=True)
-        self.__engine = create_engine(f'sqlite:///{database_file}', echo=False)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine))
-
-
-    def get_all_hotels(self):
-        query = select(Hotel)
-        hotels = self.__session.execute(query).scalars().all()
-        return hotels
-
-    def get_hotels_by_name(self, name):
-        query = select(Hotel).where(Hotel.name.like(f"%{name}%"))
-        hotels = self.__session.execute(query).scalars().all();
-        return hotels
-
-
-
-if __name__ == "__main__":
-    sm = SearchManager('../data/database.db')
-    hotels = sm.get_all_hotels()
-    for hotel in hotels:
-        print(hotel)
-
-    name = input("Enter hotel name: ")
-    hotels = sm.get_hotels_by_name(name)
-    for hotel in hotels:
-        print(hotel)
-'''
 class SearchManager:
 
     def accept_search_criteria(self):
         criteria = []
         return criteria
 
-    def show_available_hotels_by_name(self, hotel_name):
-        query = select(Hotel).where(Hotel.name.like('%Amaris'))
-
-
-    def search_hotel_by_stars(self, stars):
-        query = select(Hotel)
-        print(query)
-        result = session.execute(query)
-        for hotel in result:
-            print(hotel)  # Notice that it will be a tuple, and not the object!
-        input("Press Enter to continue...")
-        print("\n")
-'''
+    def show_available_hotels(self, criteria):
+        pass
--- main_console.py
+++ main_console.py
@@ -1,15 +1,10 @@
 # from search import SearchManager
 # from register import UserManager
-from sqlalchemy import select
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
-
-
-from business.SearchManager import SearchManager
 from data_access import data_loader as dl
 from data_access.data_base import *
-from main_hotel_mgn import HotelManager
 
+
+DB_PATH = './data/hotel_reservation.db'
 
 def load_db():
     # Import relevant data classes and initialize objects for hotels, registered users, admin users and other
@@ -38,8 +33,6 @@ def navigate():
         case '0':
             print("Search")
             # call functions in SearchManager
-            searchMgr = SearchManager()
-            searchMgr.search_hotel_by_stars()
         case '1':
             print("Register")
             # call functions in UserManager
@@ -47,53 +40,23 @@ def navigate():
             print("No such option, please enter a valid choice as shown in the Menu")
             choice = input("Choose an option for your desired action: ")
 
-DB_FILE = './data/hotel_reservation.db'
-GENERATE_EXAMPLES = False
+
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     # later replace with load_sqlite_db()
     #load_db()
-    db_path = Path(DB_FILE)
-    if not db_path.parent.exists():
-        db_path.parent.mkdir()
 
-    engine = create_engine(f'sqlite:///{db_path}')
-
-    if db_path.exists():
-        if GENERATE_EXAMPLES:
-            Base.metadata.drop_all(engine)
-            Base.metadata.create_all(engine)
-            init_db(DB_FILE, False, True, True)
-    else:
-        Base.metadata.create_all(engine)
-
-    session = scoped_session(sessionmaker(bind=engine))
-
-    hotel_mgn = HotelManager(session)
-
-    query = select(Hotel)
-    hotels = session.execute(query).scalars().all()
-    print(hotels)
-
-
-
-
-
-
-
-
+    init_db(DB_PATH, True, True, True)
 
     # try:
     #     dl.load_data_from_sqlite()
     # except Exception as e:
     #     print("There was a problem in loading the database, please fix the error and try again: ", e)
     # later replace with logic to show the home page GUI of your hotel
-    #show_welcome()
+    show_welcome()
 
     # can be replaced by GUI control elements to create a simple menu on the home page.
-    #show_menu()
+    show_menu()
 
     # can be replaced with individual screens for different function like register, login, search etc.
     #navigate()
-
-    session.close()
