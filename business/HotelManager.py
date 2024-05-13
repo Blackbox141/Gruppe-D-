0816d974ee04595680515871f33bb89d9c8a188d
SlowMoLuc <luca.lenherr@students.fhnw.ch>
2024-05-13 12:39:59 +0200

    Changes by dennis

--- business/HotelManager.py
+++ business/HotelManager.py
@@ -1,53 +1,74 @@
+import sys
 from pathlib import Path
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy import create_engine, select
 
+from business.BaseManager import BaseManager
 from data_access.data_base import init_db
 from data_models.models import Hotel, Address, Room
 
-class HotelManager:
-    def __init__(self, database_file):
-        database_path = Path(database_file)
-        if not database_path.is_file():
-            init_db(database_file, generate_example_data=True)
-        self.__engine = create_engine(f'sqlite:///{database_file}', echo=False)
-        self.__session = scoped_session(sessionmaker(bind=self.__engine))
+class HotelManager(BaseManager):
 
-    def add_hotel(self):
-        hotel_name_input = str(input("Hotel Name: "))
-        hotel_stars_input = int(input("Hotel Stars: "))
-        street_input = str(input("Street: "))
-        zip_input = int(input("Zip Code: "))
-        city_input = str(input("City: "))
-        room_number_input = int(input("Room number: "))
-        room_type_input = input("Enter room type: ")
-        room_max_guests_input = input("Room max. guests: ")
-        room_description_input = input("Room description: ")
-        room_amenities_input = input("Amenities: ")
-        room_price_input = float(input("Price per night: "))
-
+    def add_hotel(self, hotel_name, hotel_stars, street, zip, city, rooms):
         hotel = Hotel(
-            name=hotel_name_input,
-            stars=hotel_stars_input,
-            address=Address(street=street_input, 
-                            zip=zip_input, 
-                            city=city_input),
-            rooms=[Room(number=room_number_input, 
-                        type=room_type_input,
-                        max_guests=room_max_guests_input, 
-                        description=room_description_input,
-                        amenities=room_amenities_input, 
-                        price=room_price_input)]
+            name=hotel_name,
+            stars=hotel_stars,
+            address=Address(street=street,
+                            zip=zip,
+                            city=city),
+            rooms=rooms
         )
 
-        self.__session.add(hotel)
-        self.__session.commit()
+        self._session.add(hotel)
+        self._session.commit()
 
-        query = select(Hotel).where(Hotel.name == hotel_name_input)
-        result = self.__session.execute(query).scalars().all()
+        query = select(Hotel).where(Hotel.name == hotel_name)
+        result = self._session.execute(query).scalars().all()
         print(result)
 
+    def delete_hotel(self):
+        pass
+
+    def update_informations(self):
+        pass
+
+    def validate(self, ask_input, error_msg, type):
+        while True:
+            user_input = input(ask_input)
+            try:
+                user_input = type(user_input)
+                return user_input
+            except ValueError:
+                print(error_msg)
+
+    def validate_in_range(self, ask_input, error_msg, type, min, max):
+        while True:
+            user_input = self.validate(ask_input, error_msg, type)
+            if user_input >= min and user_input <= max:
+                return user_input
+            else: print(error_msg)
 
 if __name__ == "__main__":
     manager = HotelManager("../data/hotel_reservation.db")
-    manager.add_hotel()
+
+    hotel_name_input = manager.validate("Hotel Name: ", "Invalid Hotel Name!", str)
+    hotel_stars_input = manager.validate_in_range("Hotel Stars: ", "Invalid Stars!", int, 0,5)
+    street_input = manager.validate("Street: ", "Invalid Street!", str)
+    zip_input = manager.validate("Zip Code: ", "Invalid Zip Code!", str)
+    city_input = manager.validate("City: ", "Invalid City!", str)
+
+    room_number_input = manager.validate_in_range("Room number: ", "Invalid Room Number!", int, 1, sys.maxsize)
+    room_type_input = manager.validate("Enter room type: ", "Invalid Room Type!", str)
+    room_max_guests_input = manager.validate_in_range("Room max. guests: ", "Invalid Guest Number!", int, 1, sys.maxsize)
+    room_description_input = manager.validate("Room description: ", "Invalid Room Description!", str)
+    room_amenities_input = manager.validate("Amenities: ", "Invalid Amenities!", str)
+    room_price_input = manager.validate_in_range("Price: ", "Invalid Price!", float, 0, sys.maxsize)
+
+    rooms = [Room(number=room_number_input,
+                        type=room_type_input,
+                        max_guests=room_max_guests_input,
+                        description=room_description_input,
+                        amenities=room_amenities_input,
+                        price=room_price_input)]
+
+    manager.add_hotel(hotel_name_input, hotel_stars_input, street_input, zip_input, city_input, rooms)
