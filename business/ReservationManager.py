28441190f761afdb55d986d987414d372034cd28
Your Name <you@example.com>
2024-05-26 15:39:15 +0200

    hallo

--- business/ReservationManager.py
+++ business/ReservationManager.py
@@ -1,3 +1,21 @@
-# include all functions related to reservations here
-# make reservation, retrieve all reservations for a hotel, retrieve reservations for a user
-# check for appropriate user roles inside the functions
+from pathlib import Path
+
+from sqlalchemy import create_engine, select, Sequence
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+from data_access.data_base import init_db
+from data_models.models import *
+
+class ReservationManager(object):
+    def __init__(self, database_path: Path):
+        self.__engine = create_engine(f'sqlite:///{database_path}', echo=False)
+        self.__session = scoped_session(sessionmaker(bind=self.__engine))
+
+    def show_all_hotels(self) -> None:
+        all_hotels = self.__rm.get_all_hotels()
+        self.show_hotels(all_hotels)
+    def get_all_rooms(self) -> list[Room]:
+        query = select(Room)
+        result = self.__session.execute(query).scalars().all()
+        return result
+
