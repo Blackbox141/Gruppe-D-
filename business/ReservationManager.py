7355c6aa555ea3a4b470feae106865453f37f8eb
Your Name <you@example.com>
2024-05-27 08:20:09 +0200

    hallo

--- business/ReservationManager.py
+++ business/ReservationManager.py
@@ -11,12 +11,40 @@ class ReservationManager(object):
         self.__engine = create_engine(f'sqlite:///{database_path}', echo=False)
         self.__session = scoped_session(sessionmaker(bind=self.__engine))
 
-    def show_all_hotels(self) -> None:
+    def get_all_hotels(self) -> None:
         all_hotels = self.__rm.get_all_hotels()
         self.show_hotels(all_hotels)
+
+    def show_hotels(self, hotels: list[Hotel]) -> None:
+        for hotel in hotels:
+            print(f'Name: {hotel.name}, Location: {hotel.location}')
+
+
+#all rooms from specific hotel?
     def get_all_rooms(self) -> list[Room]:
         query = select(Room)
         result = self.__session.execute(query).scalars().all()
         return result
-#hello
+
+#check for login
+    def check_login(self, user_id: int):
+        query = select(User).where(User.id == user_id)
+        result = self.__session.execute(query).scalars().first()
+        return result
+#reservation
+    def book_room(self, user_id: int, room_id: int):
+        room = self.__session.get(Room, room_id)
+        if not room:
+            return f"Room {room_id} does not exist."
+
+        if room.already_booked:
+            return f"Room {room_id} is already booked."
+
+        reservation = Reservation(user_id=user_id, room_id=room_id)
+        room.is_booked = True
+        self.__session.add(reservation)
+        self.__session.commit()
+        return f"Room {room_id} has been successfully booked."
+
+    def cancel_reservation(self, user_id: int, room_id: int):
 
