2f0ea92fa0b0acc1468c8cc15dee4288f0b4661a
SlowMoLuc <luca.lenherr@students.fhnw.ch>
2024-05-30 16:05:06 +0200

    hello :)

---
+++ UserManagerMain.py
@@ -0,0 +1,25 @@
+import sys
+
+from business.TestUserManager import UserManager
+
+if __name__ == '__main__':
+    user_manager = UserManager("./data/hotel_reservation.db")
+    while user_manager.has_more_attempts():
+        username = input("Enter your username: ")
+        password = input("Enter your password: ")
+        if user_manager.login(username, password):
+            break
+        else:
+            print("Invalid username or password")
+    if user_manager.get_current_user():
+        print("Logged in successfully")
+        print(f"Welcome {user_manager.get_current_user().username}")
+        if user_manager.is_current_user_admin():
+            print("You have granted admin rights")
+        else:
+            print("You are a registered guest")
+            reg_guest = user_manager.get_reg_guest_of(user_manager.get_current_user())
+            print(f"{reg_guest.firstname} {reg_guest.lastname}")
+    else:
+        print("Too many attempts")
+
---
+++ business/TestUserManager.py
@@ -0,0 +1,44 @@
+import sys
+
+from sqlalchemy import select
+from sqlalchemy.sql.functions import current_user
+
+from data_models.models import Login, RegisteredGuest
+from business.BaseManager import BaseManager
+
+class UserManager(BaseManager):
+    def __init__(self, db_file):
+        super(UserManager, self).__init__(db_file)
+        self._attempts = 0
+        self._current_user = None
+
+    def login(self, username, password):
+        query = select(Login).where(Login.username == username, Login.password == password)
+        result = self._session.execute(query).scalar_one_or_none()
+        self._attempts += 1
+        self._current_user = result
+        return result
+    
+    def has_more_attempts(self):
+        if self._attempts < 3:
+            return True
+        else:
+            return False
+
+    def logout(self):
+        self._current_user = None
+        self._attempts = 0
+
+    def get_current_user(self) -> Login:
+        return self._current_user
+
+    def get_reg_guest_of(self, login) -> RegisteredGuest:
+        query = select(RegisteredGuest).where(RegisteredGuest.login_id == login.id)
+        result = self._session.execute(query).scalar_one_or_none()
+        return result
+
+    def is_current_user_admin(self):
+        if self._current_user:
+            if self._current_user.role.access_level == sys.maxsize:
+                return True
+        return False
